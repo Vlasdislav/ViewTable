1542df6e26923d443966b29463adb2e4cfdb6919
Vlasdislav <v.shchetinin.v@gmail.com>
2024-08-22 12:12:08 +0300

    access to the domain is open

--- backend/src/main/java/com/example/tableview/config/WebConfig.java
+++ backend/src/main/java/com/example/tableview/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8085")
+                        .allowedOrigins("http://127.0.0.1:8085")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
